26b5216db2124e73dda6d614a97a390f3b9d14ab
EvanBradford <evan.bradford1@gmail.com>
2019-07-05 01:16:23 -0400

    Everything is theoretically finished but still getting 500 errors and I don't know what to do

---
+++ src/main/java/servlets/AddReimbursementServlet.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ReOracle;
+
+/**
+ * Servlet implementation class AddUserServlet
+ */
+public class AddReimbursementServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = -6545567095900943875L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		double amount = Double.parseDouble(request.getParameter("amount"));
+		String purpose = request.getParameter("purpose");//"adam";
+		String date = request.getParameter("date");//"adam";
+		String status = "SUBMITTED";
+		int id = Integer.parseInt(request.getParameter("id"));//"adam@gmail.com";
+		
+		ReOracle userDAO = new ReOracle();
+		try {
+			userDAO.insertRe(amount, purpose, date, status, id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+}
