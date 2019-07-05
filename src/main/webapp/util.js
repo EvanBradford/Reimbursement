15e32bc5b3ca1857aaa7a9fcd8eeab084286b5da
EvanBradford <evan.bradford1@gmail.com>
2019-07-05 10:21:33 -0400

    Made my html actually look sort of decent ish

--- src/main/webapp/util.js
+++ src/main/webapp/util.js
@@ -13,3 +13,22 @@ function ajax(url,method,callback){
 	xhr.open("GET", url, true);
 	xhr.send();
 }
+function ajax2(url, method){
+	var promiseObj = new Promise(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200)
+			{
+				var response = this.responseText;
+				resolve(response);
+			}
+			else if(this.readyState == 4 && this.status != 200)
+			{
+				reject(response);
+			}
+		}
+        xhr.open(method, url, true);
+        xhr.send();
+    });
+    return promiseObj;
+}
