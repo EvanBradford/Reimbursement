75fef648067be1b97b03b78d9f299cf59f134142
EvanBradford <evan.bradford1@gmail.com>
2019-07-08 01:50:59 -0400

    Finished some more things, something broke

---
+++ src/main/java/servlets/DenyServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ReOracle;
+
+/**
+ * Servlet implementation class DenyServlet
+ */
+public class DenyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ReOracle userDAO = new ReOracle();
+		int reId = Integer.parseInt(request.getParameter("id"));
+		try {
+			userDAO.deny(reId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
